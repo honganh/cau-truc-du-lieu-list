f5b21e2a7a0ce489d651e02bef34fe3828e058ec
Hong Anh <pndhainam@hotmail.com>
2016-02-25 17:39:21 +0700

    Update Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -37,12 +37,12 @@ void Node::print()
 {
     printf("\nX:%d,Y:%d,VALUE:%d",this->X,this->Y,this->value);
 }
-int Node::get_X(){return this->X ;}
-int Node::get_Y(){return this->Y ;}
-int Node::get_VALUE(){return this->value ;}
-void Node::set_X(int x){this->X = x ;}
-void Node::set_Y(int y){this->Y = y ;}
-void Node::set_VALUE(int value){this->value = value ;}
+int Node::getX(){return this->X ;}
+int Node::getY(){return this->Y ;}
+int Node::getValue(){return this->value ;}
+void Node::setX(int x){this->X = x ;}
+void Node::setY(int y){this->Y = y ;}
+void Node::setValue(int value){this->value = value ;}
 void Node::set(int x, int y ,int value){
     this->X = x;this->Y = y ;this->value = value ;
 }
