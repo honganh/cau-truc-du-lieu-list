cd6c9430c0e7b3cc4c43a0502ca9aa61794100ac
Hong Anh <pndhainam@hotmail.com>
2016-02-25 17:37:33 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,11 +10,14 @@
 
 int main(int argc ,char **argv)
 {
-   if(argc < 4){
+   if(argc < 4)
+   {
         printf("\n Error input .");
         printf("\n Input follow : <program name> <path_file_input> <path_file_output1> <path_file_output2>");
         return -1;
-    }else{
+    }
+    else
+    {
         char *input = argv[1];
         char *output_1 = argv[2];
         char *output_2 = argv[3];
