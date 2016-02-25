eaef15ceb5673afd980592f62f5949b40833f15b
Hong Anh <pndhainam@hotmail.com>
2016-02-25 17:42:52 +0700

    Update Stack.cpp

--- src/Stack.cpp
+++ src/Stack.cpp
@@ -2,14 +2,18 @@
 
 Stack::Stack():Dlinkedlist(){}
 Stack::~Stack(){}
-Node* Stack::remove(){
-    if(this->isempty()){
+Node* Stack::remove()
+{
+    if( this->isempty() )
+    {
         return NULL;
-    }else{
+    }
+    else
+    {
         Node *tmp = new Node();
-        tmp = this->head->get_pnext();
-        (tmp->get_pnext())->set_prev(this->head);
-        this->head->set_pnext(tmp->get_pnext());
+        tmp = this->head->getPnext();
+        (tmp->get_pnext())->setPrev(this->head);
+        this->head->setPnext(tmp->getPnext());
         this->size--;
         return tmp;
     }
